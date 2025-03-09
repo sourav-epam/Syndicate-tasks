7f6b4a0707ef37c4b4fc75246e69231f80cda472
Sourav Chandra Dey <souravchandra_dey@epam.com>
2025-03-09 16:23:14 +0530

    task 04

---
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -0,0 +1,30 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent.SNS;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent.SNSRecord;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@LambdaHandler(lambdaName = "sns_handler", roleName = "sns_handler-role", isPublishVersion = true, aliasName = "${lambdas_alias_name}", logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED)
+public class SnsHandler implements RequestHandler<SNSEvent, List<String>> {
+
+	@Override
+	public List<String> handleRequest(SNSEvent event, Context context) {
+		LambdaLogger logger = context.getLogger();
+		logger.log("EVENT TYPE: " + event.getClass().toString());
+		var messagesFound = new ArrayList<String>();
+		for (SNSRecord record : event.getRecords()) {
+			SNS message = record.getSNS();
+			messagesFound.add(message.getMessage());
+		}
+		return messagesFound;
+	}
+}
---
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -0,0 +1,29 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@LambdaHandler(lambdaName = "sqs_handler", roleName = "sqs_handler-role", isPublishVersion = true, aliasName = "${lambdas_alias_name}", logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED)
+
+public class SqsHandler implements RequestHandler<SQSEvent, List<String>> {
+
+	@Override
+	public List<String> handleRequest(SQSEvent event, Context context) {
+		LambdaLogger logger = context.getLogger();
+		logger.log("EVENT TYPE: " + event.getClass().toString());
+		var messagesFound = new ArrayList<String>();
+		for (SQSMessage msg : event.getRecords()) {
+			messagesFound.add(msg.getBody());
+		}
+		return messagesFound;
+	}
+}
