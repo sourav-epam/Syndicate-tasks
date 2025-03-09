9ab0999510a90a0335ab7bb639db0fbae3b099a0
Sourav Chandra Dey <souravchandra_dey@epam.com>
2025-03-09 23:53:30 +0530

    added trigger events

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -2,7 +2,10 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
+import com.syndicate.deployment.annotations.events.SnsEvents;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RegionScope;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
@@ -14,6 +17,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @LambdaHandler(lambdaName = "sns_handler", roleName = "sns_handler-role", isPublishVersion = true, aliasName = "${lambdas_alias_name}", logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED)
+
+@SnsEventSource(regionScope = RegionScope.DEFAULT, targetTopic = "lambda_topic")
+@SnsEvents
 public class SnsHandler implements RequestHandler<SNSEvent, List<String>> {
 
 	@Override
--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -2,6 +2,8 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.events.SqsEvents;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
@@ -12,8 +14,16 @@ import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
 import java.util.ArrayList;
 import java.util.List;
 
-@LambdaHandler(lambdaName = "sqs_handler", roleName = "sqs_handler-role", isPublishVersion = true, aliasName = "${lambdas_alias_name}", logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED)
+@LambdaHandler(
+	lambdaName = "sqs_handler", 
+	roleName = "sqs_handler-role", 
+	isPublishVersion = true, 
+	aliasName = "${lambdas_alias_name}", 
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
 
+@SqsTriggerEventSource(targetQueue = "async_queue", batchSize = 123)
+@SqsEvents
 public class SqsHandler implements RequestHandler<SQSEvent, List<String>> {
 
 	@Override
