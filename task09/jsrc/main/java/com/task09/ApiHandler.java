3257fdec730c29366319beea5749d4011eede19b
Sourav Chandra Dey <souravchandra_dey@epam.com>
2025-03-13 16:33:11 +0530

    added lib folder to task09

--- task09/jsrc/main/java/com/task09/ApiHandler.java
+++ task09/jsrc/main/java/com/task09/ApiHandler.java
@@ -3,8 +3,13 @@ package com.task09;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.HttpUrl;
@@ -17,9 +22,26 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@LambdaHandler(lambdaName = "api_handler", roleName = "api_handler-role", isPublishVersion = true, aliasName = "${lambdas_alias_name}", logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED)
-
-@LambdaUrlConfig()
+@LambdaHandler(
+	lambdaName = "api_handler", 
+	roleName = "api_handler-role", 
+	isPublishVersion = true, 
+	aliasName = "${lambdas_alias_name}", 
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@LambdaLayer(
+	layerName = "weather_sdk", 
+	libraries = {
+		"lib/sdk-1.10.0.jar" }, 
+		runtime = DeploymentRuntime.JAVA11, 
+		artifactExtension = ArtifactExtension.ZIP
+)
+		
+@LambdaUrlConfig(
+	authType = AuthType.NONE, 
+	invokeMode = InvokeMode.BUFFERED
+)
+@SuppressWarnings("unchecked")
 public class ApiHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
 	private static final String BASE_URL = "https://api.open-meteo.com/v1/forecast";
@@ -68,7 +90,7 @@ public class ApiHandler implements RequestHandler<Map<String, Object>, Map<Strin
 
 	static class WeatherClient {
 
-        public Map<String, Object> getWeather(double latitude, double longitude) throws IOException {
+		public Map<String, Object> getWeather(double latitude, double longitude) throws IOException {
             HttpUrl url = HttpUrl.parse(BASE_URL)
                     .newBuilder()
                     .addQueryParameter("latitude", String.valueOf(latitude))
